6069ea33094a7f2f14ac2372752c99307cf85654
Luís A. Bastião Silva <luis.kop@gmail.com>
2012-09-18 15:21:42 +0100

    Adding the structureal classes.
    It includes the DBQueue implementation and also files to unit testing.

---
+++ src/main/java/org/koplabs/dbqueue/DBQueue.java
@@ -0,0 +1,107 @@
+/*  Copyright   2012 - Luís A. Bastião Silva
+ *
+ *  This file is part of JDBQueue.
+ *
+ *  JDBQueue is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JDBQueue is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with PACScloud.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.koplabs.dbqueue;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+/**
+ *
+ * @author Luís A. Bastião Silva <luis.kop@gmail.com>
+ */
+public class DBQueue implements Queue
+{
+    
+    
+
+    public boolean add(Object e) 
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean offer(Object e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object remove() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object poll() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object element() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object peek() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Iterator iterator() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object[] toArray() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object[] toArray(Object[] a) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean containsAll(Collection c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean addAll(Collection c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean removeAll(Collection c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean retainAll(Collection c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
