6590583f660ef722294c938f761c8f609e4cefc8
Luís A. Bastião Silva <luis.kop@gmail.com>
2012-09-25 11:24:11 +0100

    Added license header file.

--- src/test/java/org/koplabs/dbqueue/WorkerTest.java
+++ src/test/java/org/koplabs/dbqueue/WorkerTest.java
@@ -1,3 +1,21 @@
+/*  Copyright   2012 - Luís A. Bastião Silva
+ *
+ *  This file is part of JDBQueue.
+ *
+ *  JDBQueue is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JDBQueue is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with PACScloud.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.koplabs.dbqueue;
 
 import java.util.logging.Level;
@@ -8,7 +26,7 @@ import org.koplabs.dbqueue.worker.JDBWorker;
 
 /**
  *
- * @author bastiao
+ * @author Luís A. Bastião Silva <luis.kop@gmail.com>
  */
 public class WorkerTest {
     
